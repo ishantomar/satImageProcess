53cfab8facd63ee0697ebdef7ba502efda319797
Ishan  Tomar <ishantomar@Ishans-MacBook-Pro.local>
2023-07-13 19:37:31 +0530

    New files added

--- image_clip.py
+++ image_clip.py
@@ -11,14 +11,14 @@ class ImageClip:
         self.b8apath = input("Band 8A path: ")
         self.b11path = input("Band 11 path: ")
 
-    def clip_raster(self, inputFIle = False)
+    def clip_raster(self, inputFIle = False, noData=-1)
         fileName = input("Path to image file to clip: ")
         outFname = input("Folder path to save the image file") 
         with fiona.open(self.shpPath, 'r') as shapeFile:
             for feature in shapeFile:
                 shapes = [feature['geometry']]
         with rasterio.open(fileName) as src:
-            out_image, out_transform = rasterio.mask.mask(src, shapes, crop=True)
+            out_image, out_transform = rasterio.mask.mask(src, shapes, crop=True, filled=True, nodata=noData)
             out_meta = src.meta
         out_meta.update({
             'height': out_image.shape[1],
@@ -27,3 +27,37 @@ class ImageClip:
         })
         with rasterio.open(outFname, 'w', **out_meta) as dst:
             dst.write(out_image)
+    def getNDVI(self, inputFile = False):
+        if inputFIle == False:
+            nirB = input("Give file name for NIR Band: ")
+            rB = input("Give the file name for Red Band ")
+        else: 
+            nirb = self.b8apath
+            rb = self.b4path
+        nirImg = rasterio.open(nirB)
+        rImg = rasterio.open(rB)
+        #Check if bounds are same 
+        # Code here
+        nirArray = nirImg.read(1)
+        rArray = rImg.read(1)
+        ndviArray = (nirArray-rArray)/(nirArray+rArray)
+        out_meta = nirImg.meta
+        with rasterio.open(outFname, 'w', **out_meta) as dst:
+            dst.write(ndviArray)
+    def getNDWI(self, inputFile=False):
+        if inputFIle == False:
+            b8a = input("Give file name for NIR Band: ")
+            b11 = input("Give the file name for Red Band ")
+        else: 
+            b8a = self.b8apath
+            b11 = self.b4path
+        nirImg = rasterio.open(b8a)
+        rImg = rasterio.open(b11)
+        #Check if bounds are same 
+        # Code here
+        nirArray = nirImg.read(1)
+        rArray = rImg.read(1)
+        ndviArray = (nirArray-rArray)/(nirArray+rArray)
+        out_meta = b8a.meta
+        with rasterio.open(outFname, 'w', **out_meta) as dst:
+            dst.write(ndviArray)
